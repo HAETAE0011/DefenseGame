3cfa68780ba75b51bf8a3b82b07795554e57b5d2
HAETAE0011 <john0011@naver.com>
2019-12-02 01:54:17 -0500

    update 2

---
+++ DefenseGame/Assets/Editor/ChangeShader_Window.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ChangeShader_Window : EditorWindow {
+    Material mat;
+    [MenuItem("Custom/Shader Changer")]
+    
+    public static void ShowWindow()
+    {
+        GetWindow<ChangeShader_Window>("Shader Changer");
+        
+    }
+
+    // Window display items
+    private void OnGUI()
+    {
+        GUILayout.Label("change the shader of the selected objects to Standard", EditorStyles.boldLabel);
+        mat = new Material(Shader.Find("Standard"));
+        if (GUILayout.Button("Change Shader to Standard")) {
+            ShaderChanger(mat);
+        }
+        GUILayout.Label("change the shader of the selected objects to Unlit", EditorStyles.boldLabel);
+        mat = new Material(Shader.Find("Unlit/Color"));
+        if (GUILayout.Button("Change Shader to Unlit"))
+        {
+            ShaderChanger(mat);
+        }
+
+    }
+
+    void ShaderChanger(Material mat) {
+        foreach (GameObject g in Selection.gameObjects) {
+            Color color = g.GetComponent<Renderer>().sharedMaterial.color;
+            g.GetComponent<Renderer>().material = mat;
+            mat.color = color;
+        }
+    }
+
+    // Methods
+
+
+
+
+
+}
---
+++ DefenseGame/Assets/Script/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100)) {
+                if (hit.collider.gameObject.tag == ("MovePoint")) {
+                    
+                    Instantiate<GameObject>(prefab, (hit.collider.gameObject.transform.position), Quaternion.Euler(-90,0,0));
+                }
+            }
+        }
+    }
+
+    
+}
